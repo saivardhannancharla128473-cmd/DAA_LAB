663d799d00960cf88b0554e5a468b59f25abb246
Saivardhan <saivardhan.nancharla128473@marwadiuniversity.ac.in>
2026-08-27 11:31:29 +0530

    Added 5 practical

--- Practical -4/Factorial.java
+++ Practical -4/Factorial.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class factorial {
+public class Factorial {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -9,5 +9,6 @@ public class factorial {
             fact = fact * i;
         }
         System.out.println("Factorial of " + n + ": " + fact);
+        
     }
 }
